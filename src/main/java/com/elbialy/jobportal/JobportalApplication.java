9282551ce9e5be558983cce47258a30aa16d0c1f
Elbialy0 <152164768+Elbialy0@users.noreply.github.com>
2025-01-23 11:53:13 +0200

    Setup the project

---
+++ src/main/java/com/elbialy/jobportal/JobportalApplication.java
@@ -0,0 +1,13 @@
+package com.elbialy.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalApplication.class, args);
+	}
+
+}
